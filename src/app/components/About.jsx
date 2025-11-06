20dfe2a602234a532e23757eb0eabf53f6a10a1e
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2025-11-06 10:53:11 +0100

    Age and education update

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -2,42 +2,43 @@ import icon from "../assets/icon.svg";
 import Image from "next/image";
 
 export default function About() {
-	return (
-		<section className="min-h-block bg-base-dark text-base-white">
-			<div className="md:max-w-7xl mx-auto  min-h-block grid md:grid-cols-2 grid-cols-1 place-items-center p-4">
-				<Image
-					className="md:m-8 aspect-video md:size-96 hidden md:block"
-					src={icon}
-					alt="Logo"
-				/>
-				<div className="md:order-1 order-first">
-					<h2 className="md:text-6xl text-4xl mb-4 md:mb-12 font-bold text-center uppercase">
-						About me
-					</h2>
-					<p className="leading-6.5">
-						Hi! My name is Janus, i am 27 years old and i am a
-						multimediadesigner/frontend developer. I pride myself in
-						creating sleek, beautiful and easy to use solutions for
-						customers and friends alike, and I believe that
-						communication is the key to a successful product.
-					</p>
-					<Image
-						className="md:m-8 aspect-video md:hidden mx-auto self-center"
-						src={icon}
-						alt=""
-					/>
-					<p className="leading-6.5">
-						<br />I have always been interested in technology and
-						learning how things work. This has made it easier for me
-						to learn new things, whether it's a new framework,
-						program, language or whatever it might be - i believe
-						that i can figure it out. I also like creating small
-						sites for fun, like quizzes or games. I like
-						communicating and connecting with people, and i really
-						enjoy meeting new people.
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section className="min-h-block bg-base-dark text-base-white">
+            <div className="md:max-w-7xl mx-auto  min-h-block grid md:grid-cols-2 grid-cols-1 place-items-center p-4">
+                <Image
+                    className="md:m-8 aspect-video md:size-96 hidden md:block"
+                    src={icon}
+                    alt="Logo"
+                />
+                <div className="md:order-1 order-first">
+                    <h2 className="md:text-6xl text-4xl mb-4 md:mb-12 font-bold text-center uppercase">
+                        About me
+                    </h2>
+                    <p className="leading-6.5">
+                        Hi! My name is Janus, i am 28 years old and i am a
+                        multimediadesigner/frontend developer and web
+                        development student. I pride myself in creating sleek,
+                        beautiful and easy to use solutions for customers and
+                        friends alike, and I believe that communication is the
+                        key to a successful product.
+                    </p>
+                    <Image
+                        className="md:m-8 aspect-video md:hidden mx-auto self-center"
+                        src={icon}
+                        alt=""
+                    />
+                    <p className="leading-6.5">
+                        <br />I have always been interested in technology and
+                        learning how things work. This has made it easier for me
+                        to learn new things, whether it's a new framework,
+                        program, language or whatever it might be - i believe
+                        that i can figure it out. I also like creating small
+                        sites for fun, like quizzes or games. I like
+                        communicating and connecting with people, and i really
+                        enjoy meeting new people.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/app/components/Timeline.jsx
+++ src/app/components/Timeline.jsx
@@ -1,114 +1,140 @@
 export default function Timeline() {
-	return (
-		<ol className="relative border-s border-gray-200 dark:border-gray-700">
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					October 2019
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Beginning to learn basic HTML and CSS
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Got inspired my by job to understand the web a create
-					websites. I started making basic list sites, first with pure
-					HTML and later creating small layouts with CSS
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					January 2020
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Start of Javascript
-				</h3>
-				<p className="mb-4 text-base font-light text-gray-500 dark:text-gray-400">
-					Starting to create small scripts with node, and creating
-					small sites with timers and dates using Javascript
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					October 2020
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Start of Wordpress and PHP
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Working with troubleshooting and fixing wordpress pages on
-					my job, gave me insigt and interest in this technology, and
-					created my first wordpress website
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					April 2021
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Start of CSS frameworks
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Created my first website with bootstrap CSS
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					January 2023
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Start of education
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Quit my job at One.com, and started my Multimediadesigner
-					education at KEA
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					August 2024
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Internship
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Started my internship at Supertusch CPH. Gained experience
-					working with real customers in a real environment, both in
-					terms of design and development.
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					January 2025
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Graduation
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Finished my education in Multimediadesign with a specialty
-					in UX and Frontend Development with an exciting case, that
-					earned me a top grade and a project that i am very happy
-					with.
-				</p>
-			</li>
-			<li className="mb-10 ms-4">
-				<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-				<time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
-					January 2025
-				</time>
-				<h3 className="text-lg font-semibold text-base-white">
-					Consultation
-				</h3>
-				<p className="text-base font-light text-gray-500 dark:text-gray-400">
-					Worked with my first professional customer as an external
-					consultant for Supertusch CPH
-				</p>
-			</li>
-		</ol>
-	);
+    return (
+        <ol className="relative border-s border-gray-200 dark:border-gray-700">
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    October 2019
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Beginning to learn basic HTML and CSS
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Got inspired my by job to understand the web a create
+                    websites. I started making basic list sites, first with pure
+                    HTML and later creating small layouts with CSS
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    January 2020
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Start of Javascript
+                </h3>
+                <p className="mb-4 text-base font-light text-gray-500 dark:text-gray-400">
+                    Starting to create small scripts with node, and creating
+                    small sites with timers and dates using Javascript
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    October 2020
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Start of Wordpress and PHP
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Working with troubleshooting and fixing wordpress pages on
+                    my job, gave me insigt and interest in this technology, and
+                    created my first wordpress website
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    April 2021
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Start of CSS frameworks
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Created my first website with bootstrap CSS
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    January 2023
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Start of education
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Quit my job at One.com, and started my Multimediadesigner
+                    education at KEA
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    August 2024
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Internship
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Started my internship at Supertusch CPH. Gained experience
+                    working with real customers in a real environment, both in
+                    terms of design and development.
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    January 2025
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Graduation
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Finished my education in Multimediadesign with a specialty
+                    in UX and Frontend Development with an exciting case, that
+                    earned me a top grade and a project that i am very happy
+                    with.
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    January 2025
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Consultation
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Worked with my first professional customer as an external
+                    consultant for Supertusch CPH
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    May 2025
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Freelance
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Increased work with Supertusch CPH working basically
+                    fulltime on different projects, primarily on Webdock.io
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-light leading-none text-gray-400 dark:text-gray-500">
+                    August 2026
+                </time>
+                <h3 className="text-lg font-semibold text-base-white">
+                    Top Up
+                </h3>
+                <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                    Started my bachelors degree in Web Development at EK in
+                    Copenhagen
+                </p>
+            </li>
+        </ol>
+    );
 }
